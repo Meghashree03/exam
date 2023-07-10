fb7d6d1493e6706c7e1d548b836e016e940a5042
Meghashree <116055115+Meghashree03@users.noreply.github.com>
2023-07-10 15:58:41 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+    <h1>Destination</h1>
+     <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card ">
+                    <div class="card-header text-center bg-info">
+                        <h1>Java</h1>
+                        </div>
+                        <div class="card-body bg-danger">
+                            ghjydyughcfhjjlkljugghvh
+                        </div>
+                    <div class="card-footer bg-secondary">
+                        <button  type="button" class="btn btn-outline-warning">
+                            Read more
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> 
+    </>
+  )
+}
+
+export default Contact
