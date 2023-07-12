0d2f8ce46100cbd9b4b93e2e91aec4ca81a8c1d2
meghashree <meghapoojary385@gmail.com>
2023-07-12 17:17:12 +0530

    done

--- src/App.js
+++ src/App.js
@@ -26,29 +26,3 @@ function App(){
 }
 export default App;
 
-
-// import "bootstrap/dist/css/bootstrap.min.css"
-
-// import Home from './Components/Home';
-// import About from './Components/About';
-// import Contact from './Components/ContactUs';
-// import {BrowserRouter, Routes, Route} from "react-router-dom";
-// import Navbar from './Components/Navbar';
-
-// function App(){
-//   return(
-//     <>
-//     {/* <h1 classname="bg-primary">React</h1>
-//     <p>This is a paragraph</p> */}
-//     <BrowserRouter>
-//     <Navbar/>
-//     <Routes>
-//       <Route path ="/" element={<Home/>}/>
-//       <Route path ="/about" element={<About/>}/>
-//       <Route path ="/contact" element={<Contact/>}/>
-//       </Routes>
-//       </BrowserRouter>
-//     </>
-//   );
-// }
-// export default App;
